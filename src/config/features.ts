8e5d9cd9eae2d84728dc566ea017dff58b63aeb7
Flynance <Institucional@flynance.tec.br>
2026-05-20 22:44:34 -0300

    adicionando controle de ambiente

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,6 +1,8 @@
-export const FEATURE_COUPLE_ACCOUNT = process.env.NODE_ENV !== 'production'
+const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV
+
+export const FEATURE_COUPLE_ACCOUNT = APP_ENV !== 'production'
 export const FEATURE_EDUCATION = false
-export const FEATURE_FUTURES = process.env.NODE_ENV !== 'production'
+export const FEATURE_FUTURES = APP_ENV !== 'production'
 export const FEATURE_REPORTS_V1 = false
 
 export const FEATURES = {
